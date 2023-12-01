f313c3ba79cfe9c0635c9645ecb12b3dc0cd8b9b
luttfimohamad <luttfimohamad@gmail.com>
2023-12-01 11:20:51 +0700

    add dashboard page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,11 +1,26 @@
 import Sidebar from '@/components/sidebar/sidebar';
 import Header from '@/components/header/header-dashboard';
 import Layout from '@/components/layout/layout';
+import DashboardChard01 from '@/components/pages/dashboard/dashboard-chard-01';
+import DashboardChard02 from '@/components/pages/dashboard/dashboard-chard-02';
+import DashboardChard03 from '@/components/pages/dashboard/dashboard-chard-03';
 
 export default function Dashboard() {
   return (
     <Layout>
-      <div className="bg-slate-50">Dashboard</div>
+      <section>
+        <header>
+          <h1 className="text-2xl font-bold mb-4 text-left underline">
+            RINGKASAN BISNIS - UNIT JASA WISATA
+          </h1>
+        </header>
+
+        <section className="grid grid-cols-12 gap-6">
+          <DashboardChard01 />
+          <DashboardChard02 />
+          <DashboardChard03 />
+        </section>
+      </section>
     </Layout>
   );
 }
