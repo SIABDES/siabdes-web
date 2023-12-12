f098d1fcb54056b28e82a77935bb2d33f7144fd3
gilangsatya19 <101808832+gilangsatya19@users.noreply.github.com>
2023-12-12 20:35:25 +0700

    fixing route ledger

--- app/api/ledgers/route.ts
+++ app/api/ledgers/route.ts
@@ -24,12 +24,7 @@ export async function GET(request: NextRequest) {
 
   try {
     const res = await AxiosAuthed(session.backendTokens.accessToken).get(
-      LEDGERS,
-      {
-        params: {
-          account_id,
-        },
-      }
+      `/ledgers/${account_id}`
     );
 
     return NextResponse.json(res.data);
